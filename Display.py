332b6ff539078437460f58a24b7ccc20ecf87ac6
Shripadpote <138662968+Shripadpote@users.noreply.github.com>
2026-02-03 01:40:27 +0530

    Update Display.py

--- Display.py
+++ Display.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
 import re
 import altair as alt
@@ -176,16 +175,13 @@ def main():
         with col1:
             st.write("Select a SPOC to check tickets which needs attention" )
             df=df.iloc[:,1:]
-            gb = GridOptionsBuilder.from_dataframe(df)
-            gb.configure_selection(selection_mode="single", use_checkbox=True)
-            grid_options = gb.build()
-
-            # Display grid
-            grid_response = AgGrid(df, gridOptions=grid_options, height=200, fit_columns_on_grid_load=True)
-
-            # Get selected row
-            selected = grid_response["selected_rows"]
-
+            selected = st.dataframe(
+            df,
+            hide_index=True,
+            use_container_width=True,
+            selection_mode="single-row"#,
+            #on_select="rerun"
+            )
             st.write(selected)
             
             
