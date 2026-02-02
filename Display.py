b14eeda731d992e33d71177faf686d6f2116c976
Shripadpote <138662968+Shripadpote@users.noreply.github.com>
2026-02-03 01:42:08 +0530

    Update Display.py

--- Display.py
+++ Display.py
@@ -179,8 +179,8 @@ def main():
             df,
             hide_index=True,
             use_container_width=True,
-            selection_mode="single-row"#,
-            #on_select="rerun"
+            selection_mode="single-row",
+            on_select="rerun"
             )
             st.write(selected)
             
