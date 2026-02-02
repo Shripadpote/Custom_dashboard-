abc122409f01fb7810bd0001c4a9830466686e9d
Shripadpote <138662968+Shripadpote@users.noreply.github.com>
2026-02-02 23:11:32 +0530

    Update Display.py

--- Display.py
+++ Display.py
@@ -177,7 +177,7 @@ def main():
             on_select="rerun"
             )
             if 'selected' not in st.session_state:
-                st.session_state.selected = df.rows[0]
+                st.session_state.selected = df.iloc[0]
             
         with col2:
             SPOC_df=df[["SPOC","NEED_ATTENTION"]]
