a769ba80b0ff34a499784591a9cc89d3aaad99f6
Shripadpote <138662968+Shripadpote@users.noreply.github.com>
2026-02-02 23:23:57 +0530

    Update Display.py

--- Display.py
+++ Display.py
@@ -203,11 +203,11 @@ def main():
         
         if selected and selected.selection and selected.selection.rows:
                 row_pos = selected.selection.rows[0]
-        new_selected = df.iloc[row_pos]
+                new_selected = df.iloc[row_pos]
 
             # Only update if actually different
-        if not new_selected.equals(st.session_state.selected):
-                st.session_state.selected = new_selected
+                if not new_selected.equals(st.session_state.selected):
+                        st.session_state.selected = new_selected
         if st.session_state.selected.selection.rows:
             idx = st.session_state.selected.selection.rows[0]
             selected_value = df.iloc[idx]["SPOC"]
