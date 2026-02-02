96ce1163c0737fbf30d448054e08a70e06b93414
Shripadpote <138662968+Shripadpote@users.noreply.github.com>
2026-02-03 00:31:32 +0530

    Update Display.py

--- Display.py
+++ Display.py
@@ -216,7 +216,7 @@ def main():
                         st.session_state.selected = new_SPOC
         
         selected_value = st.session_state.selected
-        if selected_value !='':
+        if 1=1:
                 st.write("Tickets for SPOC which needs attention: ", selected_value)
         #st.dataframe(df,hide_index=True)
                 st.write('')
